81f7a6d1b62efb813dd2925d1428165502665cc2
tenspot <stopten55@gmail.com>
2018-10-12 17:14:12 +0100

    JS Game
    An example of using javascript to write a web based game.
    presented on freecodecamp youTube site: https://www.youtube.com/watch?v=3EMxBkqC4z0

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
 
+/*
+* Some example code to start...
+// clear the whole canvas
+ctx.clearRect(0, 0, 800, 600);
+
+// Draw a red square
 ctx.fillStyle = "#f00";
 ctx.fillRect(20, 20, 100, 100);
 
+// Draw a blue square
 ctx.fillStyle = "#00f";
 ctx.fillRect(200, 200, 50, 50);
+*/
