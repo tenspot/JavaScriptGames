79c14660e98599c7242a218282db28566eb3b34b
tenspot <stopten55@gmail.com>
2018-10-13 14:51:36 +0100

    Adding bricks to the game

--- src/ball.js
+++ src/ball.js
@@ -4,6 +4,8 @@ export default class Ball {
     this.gameWidth = game.gameWidth;
     this.gameHeight = game.gameHeight;
 
+    this.game = game;
+
     this.position = { x: 10, y: 10 };
     this.speed = { x: 2, y: 2 };
     this.size = 20;
@@ -23,12 +25,30 @@ export default class Ball {
     this.position.x += this.speed.x;
     this.position.y += this.speed.y;
 
+    // Wall on left or right
     if (this.position.x + this.size > this.gameWidth || this.position.x < 0) {
       this.speed.x = -this.speed.x;
     }
 
+    // Wall on top or bottom
     if (this.position.y + this.size > this.gameHeight || this.position.y < 0) {
       this.speed.y = -this.speed.y;
+    }
+
+    // Check for collision with paddle
+    let bottomOfBall = this.position.y + this.size;
+    let topOfPaddle = this.game.paddle.position.y;
+    let leftSideOfPaddle = this.game.paddle.position.x;
+    let rightSideOfPaddle =
+      this.game.paddle.position.x + this.game.paddle.width;
+
+    if (
+      bottomOfBall >= topOfPaddle &&
+      this.position.x >= leftSideOfPaddle &&
+      this.position.x + this.size <= rightSideOfPaddle
+    ) {
+      this.speed.y *= -1;
+      this.position.y = this.game.paddle.position.y - this.size;
     }
   }
 }
